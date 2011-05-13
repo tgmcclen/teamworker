42df93123b2d2f90f73f12fb4b913735760d0278
Trent McClenahan <trent.mcclenahan@gmail.com>
2011-05-13 16:39:50 -0400

    Initial Rails application commit with Devise included

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teamworker::Application.config.secret_token = '0e7368cc69ac796f8588d4104a99977b543b97301194f02639465f91d408b83298604ee59e629f600fdcf4de0d2d57161567324b8c6ba3e38180223b60bc3cda'
