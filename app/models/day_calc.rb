76ec9c6fe1762a38ad255add2d14ea56582b6b0c
Trent McClenahan <trent.mcclenahan@gmail.com>
2011-05-19 09:11:21 -0400

    Created DayCalc model object to represent all calculatons (supply fte, absences. etc) per day Added in absence calculations

---
+++ app/models/day_calc.rb
@@ -0,0 +1,76 @@
+class DayCalc
+  attr_accessor :day_id, :date, :raw_supply_fte, :absence_fte
+
+  def initialize
+    @absence_fte = 0
+    @raw_supply_fte = 0
+  end
+  
+  def self.calc_raw_supply_fte(start_date = nil, end_date = nil)
+    # AREL query - then run it throuh find_by_sql (Rails doesn't seem to properly support AREL yet!).  
+    # Once executed, the returned Days objects have a supply_fte property
+    # TODO increase this to cover off sum absences and then net supply
+    d = Day.arel_table
+    s = Supply.arel_table
+    a = Absence.arel_table
+    q = d.project('days.id, days.date, sum(supplies.fte_ratio) as raw_supply_fte')
+    .join(s, Arel::Nodes::OuterJoin).on(d[:date].gteq(s[:start_date]).and(d[:date].lteq(s[:end_date]).or(s[:end_date].eq(nil))))
+    .where(d[:work_day].eq(true))
+    .group(d[:date])
+    .order(d[:date].asc)
+    tighten_date_range(q, start_date, end_date)
+    puts q.to_sql
+    Day.find_by_sql(q.to_sql).collect do |d|
+      dc = DayCalc.new
+      dc.day_id = d.id
+      dc.date = d.date
+      dc.raw_supply_fte = d.raw_supply_fte unless d.raw_supply_fte.nil?
+      dc
+    end
+  end
+
+  def self.calc_absence_fte(start_date = nil, end_date = nil)
+    d = Day.arel_table
+    s = Supply.arel_table
+    a = Absence.arel_table
+    q = d.project('days.id, days.date, sum(supplies.fte_ratio) as absence_fte')
+    .join(a).on(d[:date].gteq(a[:start_date]).and(d[:date].lteq(a[:end_date])))
+    .join(s).on(a[:supply_id].eq(s[:id]))
+    .where(d[:work_day].eq(true))
+    .group(d[:date])
+    .order(d[:date].asc)
+    tighten_date_range(q, start_date, end_date)
+    puts q.to_sql
+    Day.find_by_sql(q.to_sql).collect do |d|
+      dc = DayCalc.new
+      dc.day_id = d.id
+      dc.date = d.date
+      dc.absence_fte = d.absence_fte unless d.absence_fte.nil?
+      dc
+    end
+  end
+
+  def self.calc_all(start_date = nil, end_date = nil)
+    days = calc_raw_supply_fte(start_date, end_date)
+    absence_days = calc_absence_fte(start_date, end_date)
+
+    # merge absence days info into day calcs
+    absence_days.each do |a|
+      d = days.detect { |d| a.day_id == d.day_id }
+      d.absence_fte = a.absence_fte
+    end
+    days
+  end
+  
+private
+  def self.tighten_date_range(q, start_date, end_date)
+    if start_date and end_date
+      q.where(d[:date].gteq(start_date).and(d[:date].lteq(end_date)))
+    elsif start_date
+      q.where(d[:date].gteq(start_date))
+    elsif end_date
+      q.where(d[:date].lteq(end_date))
+    end
+  end
+
+end
--- db/scrapbook.sql
+++ db/scrapbook.sql
@@ -1,17 +1,32 @@
 # people supply by day
 select days.date, sum(supplies.fte_ratio)
-from days INNER JOIN supplies on days.date >= supplies.start_date and (days.date <= supplies.end_date or supplies.end_date IS NULL) 
+from days
+left outer join supplies on days.date >= supplies.start_date and (days.date <= supplies.end_date or supplies.end_date IS NULL) 
 where days.work_day == 't'
 group by days.date;
 
-# AREL query - then run it throuh find_by_sql (Rails doesn't seem to properly support AREL yet!).  Once executed, the returned Days objects have a
-# supply_fte_ratio property
-d = Day.arel_table
-s = Supply.arel_table
-Day.find_by_sql(d.project('days.*, sum(supplies.fte_ratio) as supply_fte_ratio').join(s).on(d[:date].gteq(s[:start_date]).and(d[:date].lteq(s[:end_date]).or(s[:end_date].eq(nil)))).where(d[:work_day].eq(true)).group(d[:date]).order(d[:date].asc).to_sql)
+select days.date, sum(supplies.fte_ratio)
+from days 
+left outer join supplies on days.date >= supplies.start_date and (days.date <= supplies.end_date or supplies.end_date IS NULL) 
+where days.work_day == 't'
+group by days.date;
 
 # clunky old way, not using AREL and is slow - going to be deprecated in RAILS 3.1 (?)
 Day.sum('supplies.fte_ratio', :conditions => { :work_day => true }, :joins => 'INNER JOIN supplies on days.date >= supplies.start_date and (days.date <= supplies.end_date or supplies.end_date IS NULL) ', :group => 'days.date')
+
+# people absences by day
+select days.date, sum(supplies.fte_ratio)
+from days 
+inner join absences on days.date >= absences.start_date and days.date <= absences.end_date 
+inner join supplies on absences.supply_id = supplies.id
+where days.work_day == 't'
+group by days.date;
+
+select days.date, absences.id, absences.start_date, absences.end_date, supplies.id, supplies.fte_ratio
+from days 
+inner join absences on days.date >= absences.start_date and days.date <= absences.end_date 
+inner join supplies on absences.supply_id = supplies.id
+where days.work_day == 't'
 
 # people supply by month
 select strftime("%Y%m", days.date), sum(supplies.fte_ratio)
